b38c5a4bf6c56a31e9140a646e2f8f268a0cf260
Katie Lin <katie.lin@softwire.com>
2024-01-16 14:32:42 +0000

    add functionality to add new item

--- todo_app/app.py
+++ todo_app/app.py
@@ -11,3 +11,9 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html', items=items)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('item')
+    add_item(title)
+    return redirect('/')
