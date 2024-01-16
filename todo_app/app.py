79c62a8af7777078ef4e6948727fae78706a2d5b
Katie Lin <katie.lin@softwire.com>
2024-01-16 17:11:49 +0000

    Sort item list by status

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from operator import itemgetter
 from todo_app.data.session_items import *
 
 from todo_app.flask_config import Config
@@ -10,7 +11,8 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     items = get_items()
-    return render_template('index.html', items=items)
+    sorted_items = sorted(items, key=itemgetter('status'), reverse=True) 
+    return render_template('index.html', items=sorted_items)
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -25,4 +27,3 @@ def update_status(item_id):
     item['status'] = 'Completed' if item_checked else 'Not Started'
     save_item(item)
     return redirect('/')
-
