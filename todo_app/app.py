f611ffc42086e1d5e6ef5c6488d85ee5f848bc26
Katie Lin <katie.lin@softwire.com>
2024-01-24 20:13:38 +0000

    update import statement

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from operator import itemgetter
-from todo_app.data.session_items import *
+import todo_app.data.session_items as session_items
 
 from todo_app.flask_config import Config
 
@@ -10,25 +10,25 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
+    items = session_items.get_items()
     sorted_items = sorted(items, key=itemgetter('status'), reverse=True) 
     return render_template('index.html', items=sorted_items)
 
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form.get('item')
-    add_item(title)
+    session_items.add_item(title)
     return redirect('/')
 
 @app.route('/update_status/<item_id>', methods=['POST'])
 def update_status(item_id):
-    item = get_item(item_id)
+    item = session_items.get_item(item_id)
     item['status'] = 'Completed' if item['status'] == 'Not Started' else 'Not Started'
-    save_item(item)
+    session_items.save_item(item)
     return redirect('/')
 
 @app.route('/delete_item/<item_id>', methods=['POST'])
 def delete(item_id):
-    item = get_item(item_id)
-    delete_item(item)
+    item = session_items.get_item(item_id)
+    session_items.delete_item(item)
     return redirect('/')
