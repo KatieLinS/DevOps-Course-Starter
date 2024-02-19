7fa8a21955092cefc590f2e7e443ff31de393320
Katie Lin <katie.lin@softwire.com>
2024-02-19 09:07:21 +0000

    exercise 2

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,34 +1,44 @@
 from flask import Flask, render_template, request, redirect
-from operator import itemgetter
-import todo_app.data.session_items as session_items
+from todo_app.data.item import Item
+import todo_app.data.trello_items as trello_items
+import os
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
+todo_list_id = os.environ.get('TODO_LIST_ID')
+todo_list = trello_items.get_list_from_board(todo_list_id)
+
+done_list_id = os.environ.get('DONE_LIST_ID')
+done_list = trello_items.get_list_from_board(done_list_id)
+
 
 @app.route('/')
 def index():
-    items = session_items.get_items()
-    sorted_items = sorted(items, key=itemgetter('status'), reverse=True) 
-    return render_template('index.html', items=sorted_items)
+    board_items = trello_items.get_cards_from_board()
+    items = []
+    for item in board_items:
+        items.append(Item.from_trello_card(
+            item, todo_list if item["idList"] == todo_list_id else done_list))
+    return render_template('index.html', items=items)
+
 
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form.get('item')
-    session_items.add_item(title)
+    trello_items.add_item(title)
     return redirect('/')
 
-@app.route('/update_status/<item_id>', methods=['POST'])
-def update_status(item_id):
-    item = session_items.get_item(item_id)
-    item['status'] = 'Completed' if item['status'] == 'Not Started' else 'Not Started'
-    session_items.save_item(item)
+
+@app.route('/complete_item/<item_id>')
+def complete_item(item_id):
+    trello_items.move_item_to_list(item_id, done_list_id)
     return redirect('/')
 
-@app.route('/delete_item/<item_id>', methods=['POST'])
-def delete(item_id):
-    item = session_items.get_item(item_id)
-    session_items.delete_item(item)
+
+@app.route('/reopen_item/<item_id>')
+def reopen_item(item_id):
+    trello_items.move_item_to_list(item_id, todo_list_id)
     return redirect('/')
