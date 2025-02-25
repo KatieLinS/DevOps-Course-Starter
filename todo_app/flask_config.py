67f0990403dcbc2c6168da4866b88dee8a363b4f
Katie Lin <katie.lin@softwire.com>
2025-02-25 15:49:17 +0000

    exercise-13

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -7,3 +7,11 @@ class Config:
         self.SECRET_KEY = os.environ.get('SECRET_KEY')
         if not self.SECRET_KEY:
             raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+
+        self.LOG_LEVEL = os.environ.get('LOG_LEVEL')
+        if not self.LOG_LEVEL:
+            self.LOG_LEVEL = 'INFO'
+
+        self.LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
+        if not self.LOGGLY_TOKEN:
+            raise ValueError("No LOGGLY_TOKEN set for Flask application. Did you follow the setup instructions?")
